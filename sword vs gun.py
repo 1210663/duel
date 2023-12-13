02d39e4a970005c50a86ba31d35587df577c1550
1210663 <noahbradford63@gmail>
2023-12-13 12:47:08 -0800

    first commit

---
+++ sword vs gun.py
@@ -0,0 +1,28 @@
+age = 17
+print = age
+name = KeyboardInterrupt
+A = "start"
+print (A)
+B = "yes or no"
+print (B)
+C = "sword master"
+print (C)
+D = "gun master"
+print (D)
+E = "go back to menu"
+print (E)
+#double quotes are the as single quotes:A
+A = "start"
+print (A)
+C = 'heath 40'
+D = 'heath 40'
+H = 'hit'
+P = 'shield'
+print (C and D and H and P)
+S = 'speical'
+K = 'have sword master to get 5 hits to get speical and have gun master to get 10 hits to get special'
+print (K)
+W = 'have gun master does 2 pieces of damage with hit and have sword master do 1 piece of damage'
+print (W)
+Q = 'have shield block hits and have the special break the shield'
+print (Q)
